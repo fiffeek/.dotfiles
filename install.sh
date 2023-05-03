a3296497ae0c823dd1183f17ee9ff9c2d2cb286d
fiffeek <filipmikina@gmail.com>
2023-05-03 22:54:39 +0200

    Add basic i3 config, rely on hjkl, win as the standard modifier

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 HOSTS="$ROOTDIR/hosts"
 PLAYBOOK="$ROOTDIR/main.yaml"
 
-apt-get -qq update && apt-get install -y -qq ansible
-
 ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass
 
 exit 0
