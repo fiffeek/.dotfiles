d762446cb98be932824c8d3e90aeee392ecb4d1c
Filip Mikina <filipmikina@gmail.com>
2023-05-16 00:22:28 +0200

    Add base for nvim setup with lazy.nvim

---
+++ roles/nvim/files/init.lua
@@ -0,0 +1,28 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require('lazy').setup({
+    -- tokyonight colorscheme
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "night" },
+    },
+    -- pending keybinds
+    { 'folke/which-key.nvim', opts = {} },
+})
