035fcf175261ee44c11ad0651bb32dd6235cd356
Filip Mikina <filipmikina@gmail.com>
2023-05-25 00:27:39 +0200

    Add lualine, add a few bindings for vim

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -21,8 +21,44 @@ local o = vim.o
 o.background = 'dark'   -- Dark background
 o.termguicolors = true  -- Enable 256 colors
 o.signcolumn = 'yes'    -- Always show signcolumn
+-- Set highlight on search
+o.hlsearch = false
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+o.clipboard = 'unnamedplus'
+-- Save undo history
+o.undofile = true
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+--Show line numbers
+o.number = true
+
+--Show relative numbers
+o.relativenumber = true
+
+--Highlight current line
+o.cursorline = true
+
+o.cmdheight = 2
+
+--Tabs -> Spaces, Tab == 2 Spaces
+o.expandtab = true
+o.smarttab = true
+o.shiftwidth = 2
+o.tabstop = 2
+--Copy indent from previous line to newline
+o.autoindent = true
+o.cindent = true
+
+--Spelling
+o.spell = true
+o.spelllang = 'en_us'
 
 require('lazy').setup({
+    'ThePrimeagen/vim-be-good',
     -- Appearance
     {
       'folke/tokyonight.nvim',
@@ -35,6 +71,26 @@ require('lazy').setup({
         vim.cmd('colorscheme tokyonight')
       end
     },
+    {
+	    -- Set lualine as statusline
+	    'nvim-lualine/lualine.nvim',
+	    -- See `:help lualine.txt`
+	    opts = {
+		    options = {
+			    theme = 'tokyonight',
+		    },
+	    },
+    },
     -- pending keybinds
     { 'folke/which-key.nvim', opts = {} },
 })
+
+--Key binds
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--Half page, center after move
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
