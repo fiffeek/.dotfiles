89413bed955d1f8009e4422e51f373d18707e216
Filip Mikina <filipmikina@gmail.com>
2023-05-27 23:31:55 +0200

    Install TPM, configure basic colorscheme for nvim

--- roles/nvim/files/lua/config/options.lua
+++ roles/nvim/files/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.cmdheight = 0
+vim.o.showcmdloc = "statusline"
+vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+vim.opt.swapfile = false
