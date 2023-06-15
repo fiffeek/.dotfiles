6d8d40d3b53bdf94734e2fd31e623ff6038c1518
Filip Mikina <filipmikina@gmail.com>
2023-06-15 16:11:10 +0200

    Add astrojs support for treesiter and lsp

---
+++ roles/nvim/files/lua/plugins/lang/astro.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "astro" })
+      end
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        astro = {},
+      },
+      settings = {
+        astro = {},
+      },
+    },
+  },
+}
