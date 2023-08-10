ef086f73d4b331120a6880b7b903265926a1564e
Filip Mikina <fmikina@radix.trade>
2023-08-09 21:24:03 -0500

    Fix partial encryption/decryption

--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fd . --extension .vault | xargs ansible-vault decrypt --vault-id vault_pass.txt
+fd . --extension .vault | xargs -n1 echo ansible-vault decrypt --vault-id vault_pass.txt | bash
--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fd . --extension .vault | xargs ansible-vault encrypt --vault-id vault_pass.txt
+fd . --extension .vault | xargs -n1 echo ansible-vault encrypt --vault-id vault_pass.txt | bash
