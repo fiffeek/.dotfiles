4105c280f08fdf6d4df2de1c4f905021a3900aa9
Filip Mikina <fmikina@radix.trade>
2023-08-10 23:21:21 -0500

    Set up fzf with telescope

--- roles/nvim/files/work/lua/plugins/telescope.lua
+++ roles/nvim/files/work/lua/plugins/telescope.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+  },
   opts = {
     defaults = {
       file_ignore_patterns = {
@@ -13,6 +22,14 @@ return {
         additional_args = function(opts)
           return { "--hidden" }
         end,
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = 'smart_case',
       },
     },
   },
