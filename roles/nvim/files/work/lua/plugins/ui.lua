83d364349666a91ee647a695c4e35ed61df02d03
Filip Mikina <fmikina@radix.trade>
2023-08-10 23:29:22 -0500

    Add a binding to dismiss all notifications

---
+++ roles/nvim/files/work/lua/plugins/ui.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>und",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    }
+  }
+}
