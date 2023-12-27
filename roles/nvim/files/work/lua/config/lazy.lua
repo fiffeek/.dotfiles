7bd8718c4497029c1d6a678ece2c19c367b00fe9
Filip Mikina <fmikina@radix.trade>
2023-12-27 03:50:01 -0600

    Support Golang for remote work

--- roles/nvim/files/work/lua/config/lazy.lua
+++ roles/nvim/files/work/lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap" },
     { import = "lazyvim.plugins.extras.test" },
     { import = "lazyvim.plugins.extras.editor.navic" },
