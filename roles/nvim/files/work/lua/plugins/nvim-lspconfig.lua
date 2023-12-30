797b0e58f219551f281cea23b5040629315c49c9
Filip Mikina <fmikina@radix.trade>
2023-12-30 08:55:03 -0600

    Wire pylsp, ruff and pyright together
    https://www.reddit.com/r/neovim/comments/sazbw6/python_language_servers/

--- roles/nvim/files/work/lua/plugins/nvim-lspconfig.lua
+++ roles/nvim/files/work/lua/plugins/nvim-lspconfig.lua
@@ -14,6 +14,48 @@ return {
           },
         },
       },
+      ruff_lsp = {
+        init_options = {
+          settings = {
+            -- Any extra CLI arguments for `ruff` go here.
+            args = {"--line-length=120"},
+          },
+        },
+        keys = {
+          {
+            "<leader>co",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.organizeImports" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Organize Imports",
+          },
+        },
+      },
+      pylsp = {
+        settings = {
+          pylsp = {
+            builtin = {
+              installExtraArgs = {'flake8', 'pycodestyle', 'pydocstyle', 'pyflakes', 'pylint', 'yapf'},
+            },
+            plugins = {
+              jedi_completion = { enabled = false },
+              rope_completion = { enabled = false },
+              flake8 = { enabled = false },
+              pyflakes = { enabled = false },
+              pycodestyle = {
+                ignore = {'E226', 'E266', 'E302', 'E303', 'E304', 'E305', 'E402', 'C0103', 'W0104', 'W0621', 'W391', 'W503', 'W504'},
+                maxLineLength = 120,
+              },
+            },
+          },
+        },
+      },
       marksman = {},
       yamlls = {
         settings = {
@@ -27,5 +69,13 @@ return {
     },
   },
   setup = {
+    ruff_lsp = function()
+      require("lazyvim.util").lsp.on_attach(function(client, _)
+        if client.name == "ruff_lsp" then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end)
+    end,
   },
 }
