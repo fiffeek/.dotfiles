3f6c62f780365469079071bf48da5ba00f9d9b11
Filip Mikina <fmikina@radix.trade>
2024-01-04 09:12:28 -0600

    Disable pylsp

--- roles/nvim/files/work/lua/plugins/nvim-lspconfig.lua
+++ roles/nvim/files/work/lua/plugins/nvim-lspconfig.lua
@@ -37,25 +37,6 @@ return {
           },
         },
       },
-      pylsp = {
-        settings = {
-          pylsp = {
-            builtin = {
-              installExtraArgs = {'flake8', 'pycodestyle', 'pydocstyle', 'pyflakes', 'pylint', 'yapf'},
-            },
-            plugins = {
-              jedi_completion = { enabled = false },
-              rope_completion = { enabled = false },
-              flake8 = { enabled = false },
-              pyflakes = { enabled = false },
-              pycodestyle = {
-                ignore = {'E226', 'E266', 'E302', 'E303', 'E304', 'E305', 'E402', 'C0103', 'W0104', 'W0621', 'W391', 'W503', 'W504'},
-                maxLineLength = 120,
-              },
-            },
-          },
-        },
-      },
       marksman = {},
       yamlls = {
         settings = {
