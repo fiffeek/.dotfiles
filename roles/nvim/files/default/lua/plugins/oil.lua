e1b49e75a3c39ae0b42389241e285d57ac8a2578
Filip Mikina <filipmikina@gmail.com>
2024-01-21 18:55:46 +0100

    Switch neotree to oil

---
+++ roles/nvim/files/default/lua/plugins/oil.lua
@@ -0,0 +1,25 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+        "permissions",
+      },
+      skip_confirm_for_simple_edits = true,
+    },
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("oil").open()
+        end,
+        desc = "Explorer Oil",
+      },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
