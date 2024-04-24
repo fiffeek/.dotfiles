40d396098b00761058a7ffaeb2bb5e079958be93
Filip Mikina <filipmikina@gmail.com>
2024-04-24 22:56:21 +0200

    Change kitty, neovim to Kanagawa

--- roles/nvim/files/default/lua/plugins/appearance.lua
+++ roles/nvim/files/default/lua/plugins/appearance.lua
@@ -1,8 +1,20 @@
 return {
   {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        background = { -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus",
+        },
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "kanagawa",
     },
   },
 }
