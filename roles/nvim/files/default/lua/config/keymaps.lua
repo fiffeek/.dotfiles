2f733a6aeebc6c77bf9794c6ecf6a6fe40d04b44
Filip Mikina <filipmikina@gmail.com>
2024-04-24 23:36:11 +0200

    Add keymaps for yank/paste that do not overwrite the register

--- roles/nvim/files/default/lua/config/keymaps.lua
+++ roles/nvim/files/default/lua/config/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- ignore registers, kinda
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
