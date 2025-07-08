b61d93d1da73983ce4d574192a5a7f1ffe1e713b
Filip Mikina <filipmikina@gmail.com>
2025-07-08 16:46:41 +0200

    bin: update tmux sessions switch, add tool versions for asdf

--- roles/bin/files/tmux-switch.sh
+++ roles/bin/files/tmux-switch.sh
@@ -8,35 +8,35 @@ export TMUX_FZF_OPTIONS="-p -w 62% -h 38% -m"
 # https://github.com/sainnhe/tmux-fzf/blob/master/scripts/session.sh
 
 if [[ $# -eq 1 ]]; then
-	selected=$1
+  selected=$1
 else
-	current_sessions=$(tmux list-sessions -F "#S")
-	repos=$(find "$REPOS" -mindepth 1 -maxdepth 1 -printf "%f\n")
-	options=($current_sessions "${repos[@]}")
-	options=($(printf "%s\n" "${options[@]}" | sort -u))
-	# https://github.com/junegunn/fzf/issues/1693
-	selected=$(printf "%s\n" "${options[@]}" | fzf-tmux --bind=enter:replace-query+print-query $TMUX_FZF_OPTIONS --preview="tmux capture-pane -ep -t {}")
+  current_sessions=$(tmux list-sessions -F "#S")
+  repos=$(find "$REPOS" -mindepth 1 -maxdepth 1 -printf "%f\n")
+  options=($current_sessions "${repos[@]}")
+  options=($(printf "%s\n" "${options[@]}" | sort -u))
+  # https://github.com/junegunn/fzf/issues/1693
+  selected=$(printf "%s\n" "${options[@]}" | fzf-tmux --bind=enter:replace-query+print-query $TMUX_FZF_OPTIONS --preview="tmux capture-pane -ep -t {}")
 fi
 
 if [ -z $selected ]; then
-	exit 0
+  exit 0
 fi
 
-session_name=$(basename ${selected})
+session_name=$(basename ${selected} | tr . _)
 dir="$REPOS/$selected"
 # some sessions do not have clearly defined cwd
 if [ ! -d "${dir}" ]; then
-	dir="$HOME"
+  dir="$HOME"
 fi
 
 tmux_running=$(pgrep tmux)
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s $session_name -c $dir
-	exit 0
+  tmux new-session -s $session_name -c $dir
+  exit 0
 fi
 
 if ! tmux has-session -t=$session_name 2>/dev/null; then
-	tmux new-session -ds $session_name -c $dir
+  tmux new-session -ds $session_name -c $dir
 fi
 
 tmux switch-client -t $session_name
