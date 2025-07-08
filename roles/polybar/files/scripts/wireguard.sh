619bf1201a516bbdbac117eb46e24d370510325d
Filip Mikina <filipmikina@gmail.com>
2025-07-09 01:06:52 +0200

    polybar: simplify wireguard icon

--- roles/polybar/files/scripts/wireguard.sh
+++ roles/polybar/files/scripts/wireguard.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-VPN_GET_STATUS=$(nmcli -f STATE,TYPE conn show)
-VPN_STATUS="$(echo $VPN_GET_STATUS | grep 'wireguard' | cut -f1 -d' ')"
+VPN_GET_STATUS=$(nmcli -f STATE,TYPE conn show --active)
+VPN_STATUS=$(echo "$VPN_GET_STATUS" | awk '/wireguard/ { print $1 }')
 
 ICON_CONNECTED=""
 ICON_DISCONNECTED=""
 
 get_connection_status() {
-  # reports connection status and location
   if [ "$VPN_STATUS" = "activated" ]; then
     echo "$ICON_CONNECTED WG"
   else
@@ -15,6 +14,6 @@ get_connection_status() {
   fi
 }
 
-case $1 in
+case "$1" in
 "--status") get_connection_status ;;
 esac
