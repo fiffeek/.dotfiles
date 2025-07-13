62ad0935e63daca48efbb5cf239cccfe1ceea01b
Filip Mikina <filipmikina@gmail.com>
2025-07-13 23:55:59 +0200

    power: disable cpu boost on battery

--- roles/power/files/set-power-profile.sh
+++ roles/power/files/set-power-profile.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -ex
+
 performance() {
   powerprofilesctl set performance
   cpupower frequency-set -g performance
+  cpupower set --turbo-boost 1
   notify-send 'Performance mode' || true
 }
 
 powersaver() {
   powerprofilesctl set power-saver
   cpupower frequency-set -g powersave
+  cpupower set --turbo-boost 0
   notify-send 'Powersaver mode' || true
 }
 
