7698e34c5208644ba4ff051a51f197e3ea696056
Filip Mikina <filipmikina@gmail.com>
2025-07-14 01:14:45 +0200

    power: change gpu power mode on bat/ac

--- roles/power/files/set-power-profile.sh
+++ roles/power/files/set-power-profile.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
+gpu_level() { echo "$1" | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level; }
+
 performance() {
   powerprofilesctl set performance
   cpupower frequency-set -g performance
   cpupower set --turbo-boost 1
+  gpu_level "auto"
   notify-send 'Performance mode' || true
 }
 
@@ -13,6 +16,7 @@ powersaver() {
   powerprofilesctl set power-saver
   cpupower frequency-set -g powersave
   cpupower set --turbo-boost 0
+  gpu_level "low"
   notify-send 'Powersaver mode' || true
 }
 
