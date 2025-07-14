13f4dbc18b28c8fef397c6696ca730822d8ac434
Filip Mikina <filipmikina@gmail.com>
2025-07-14 20:10:03 +0200

    power: ensure xrandr modes are loaded prior to applying

--- roles/power/files/adjust-refresh-rate.sh
+++ roles/power/files/adjust-refresh-rate.sh
@@ -4,6 +4,8 @@ set -x
 
 POWER_STATUS=$(cat /sys/class/power_supply/ACAD/online)
 
+ensure-edp-modes-exist
+
 if [ "$POWER_STATUS" -eq 1 ]; then
   xrandr --verbose --output eDP --mode "2880x1920"
   notify-send "Refresh rate 120" || true
