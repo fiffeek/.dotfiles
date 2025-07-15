bf255154248429c99a17d6097d8e49c84301fea0
Filip Mikina <filipmikina@gmail.com>
2025-07-15 03:37:53 +0200

    power: wait for x to become available on startup explicitly

--- roles/power/files/ensure-edp-modes-exist.sh
+++ roles/power/files/ensure-edp-modes-exist.sh
@@ -2,6 +2,13 @@
 
 set -x
 
+for _ in {1..10}; do
+  if xrandr >/dev/null 2>&1; then
+    break
+  fi
+  sleep 1
+done
+
 OUTPUT="eDP"
 MODE_NAME="2880x1920_60.00"
 MODELINE="473.25  2880 3112 3424 3968  1920 1923 1933 1989 -hsync +vsync"
