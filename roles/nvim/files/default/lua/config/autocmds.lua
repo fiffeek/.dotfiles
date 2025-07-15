101e785ab7fb04510c4ed77b7c05970122bca755
Filip Mikina <filipmikina@gmail.com>
2025-07-15 02:27:24 +0200

    nvim: auto attach ansible lsp to ansible-like files (heuristic)

--- roles/nvim/files/default/lua/config/autocmds.lua
+++ roles/nvim/files/default/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- attach ansible lsp on ansible-like files
+vim.cmd([[
+  au BufRead *.yaml,*.yml if search('hosts:\|tasks:', 'nw') | set ft=yaml.ansible | endif
+  au BufRead,BufNewFile */roles/*.yaml set filetype=yaml.ansible
+  au BufRead,BufNewFile */playbooks/*.yaml set filetype=yaml.ansible
+]])
