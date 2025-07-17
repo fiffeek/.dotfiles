7a2e0d75359bfdf14a78353a7469e5a8ee28bb0d
Filip Mikina <filipmikina@gmail.com>
2025-07-18 00:17:50 +0200

    nvim: bump version

--- roles/nvim/files/default/lua/config/lazy.lua
+++ roles/nvim/files/default/lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.editor.navic" },
     { import = "lazyvim.plugins.extras.editor.fzf" },
     { import = "lazyvim.plugins.extras.dap" },
